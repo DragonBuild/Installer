e4aa504ca50f15497bea63d771f3a5aa01fb71bc
Hayden <me@diatr.us>
2020-06-18 21:49:49 -0400

    Add iOS support to the Install script

--- install.sh
+++ install.sh
@@ -21,6 +21,13 @@ command -v perl >/dev/null 2>&1 || need+="perl "
 command -v dpkg >/dev/null 2>&1 || need+="dpkg "
 command -v unzip >/dev/null 2>&1 || need+="unzip "
 
+iosInstall() {
+    if [ $need != "" ]; then
+      echo "Please Install the Following Dependencies (${need})."
+      exit 1
+    fi
+}
+
 macosInstall() {
     if [ $need != "" ]; then
       read -p "Using Brew To Install Dependencies (${need}). Press Enter to Continue." || exit 1
@@ -45,7 +52,11 @@ linuxInstall() {
 
 installDragonBuild() {
     distr=$(uname -s)
-    if [ $distr == "Darwin" ]; then macosInstall
+    arch=$(uname -p)
+    if [ $distr == "Darwin" ]; then 
+        if [ $arch == "arm" ] || [ $arch == "arm64" ]; then iosInstall
+        else macosInstall
+        fi
     else linuxInstall
     fi
     echo "Downloading DragonBuild..."
