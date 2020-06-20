ed6dd7fa25ae1254d3e15829844a6afbfbbe62e1
CRKatri <katri.cameron@gmail.com>
2020-06-20 00:21:09 -0400

    Fix bashisms and be failed by dash

--- install.sh
+++ install.sh
@@ -7,19 +7,21 @@ nosudo() {
 
 crd=$PWD
 
-[[ $UID == 0 || $EUID == 0 ]] && nosudo
+if [ "$(id -ru)" = "0" ] || [ "$(id -u)" = "0" ]; then
+    nosudo
+fi
 
 sudo -p "Password for installation: " printf "" || exit 1
 
 need=""
 
-command -v wget >/dev/null 2>&1 || need+="wget "
-command -v ninja >/dev/null 2>&1 || need+="ninja "
-command -v python3 >/dev/null 2>&1 || need+="python3 "
-command -v ldid >/dev/null 2>&1 || need+="ldid "
-command -v perl >/dev/null 2>&1 || need+="perl "
-command -v dpkg >/dev/null 2>&1 || need+="dpkg "
-command -v unzip >/dev/null 2>&1 || need+="unzip "
+command -v wget >/dev/null 2>&1 || need="${need} wget"
+command -v ninja >/dev/null 2>&1 || need="${need} ninja"
+command -v python3 >/dev/null 2>&1 || need="${need} python3"
+command -v ldid >/dev/null 2>&1 || need="${need} ldid"
+command -v perl >/dev/null 2>&1 || need="${need} perl"
+command -v dpkg >/dev/null 2>&1 || need="${need} dpkg"
+command -v unzip >/dev/null 2>&1 || need="${need} unzip"
 
 iosInstall() {
     if [ "$need" != "" ]; then
@@ -30,21 +32,21 @@ iosInstall() {
 
 macosInstall() {
     if [ "$need" != "" ]; then
-      read -p "Using Brew To Install Dependencies (${need}). Press Enter to Continue." || exit 1
+      read "Using Brew To Install Dependencies (${need}). Press Enter to Continue." || exit 1
       brew install $need
     fi
 }
 
 linuxInstall() {
     if [ "$need" != "" ]; then
-      read -p "Installing Dependencies (${need}). Press Enter to Continue." || exit 1
+      read "Installing Dependencies (${need}). Press Enter to Continue." || exit 1
       if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $need || failedinstall=1
        elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $need || failedinstall=1
        elif [ -x "$(command -v dnf)" ];     then sudo dnf install $need || failedinstall=1
        elif [ -x "$(command -v zypper)" ];  then sudo zypper install $need || failedinstall=1
       else failedinstall=1;
       fi
-      if [ $failedinstall == 1 ]; then
+      if [ 1 = $failedinstall ]; then
         echo "Installing dependencies failed. You need to manually install: $need">&2; 
       fi
     fi
@@ -53,21 +55,17 @@ linuxInstall() {
 installDragonBuild() {
     distr=$(uname -s)
     arch=$(uname -p)
-    if [ "$distr" == "Darwin" ]; then 
-        if [ "$arch" == "arm" ] || [ "$arch" == "arm64" ]; then iosInstall
+    if [ "Darwin" = "$distr" ]; then 
+        if [ "arm" = "$arch" ] || [ "arm64" = "$arch" ]; then iosInstall
         else macosInstall
         fi
     else linuxInstall
     fi
     echo "Downloading DragonBuild..."
     cd ~
-    git clone https://github.com/DragonBuild/DragonBuild.git
-    mv DragonBuild .dragonbuild
+    git clone --recursive https://github.com/DragonBuild/DragonBuild.git .dragonbuild
     echo "Installing DragonBuild"
-    source ~/.dragonbuild/internal/environment
-    cd ~/.dragonbuild 
-    git pull
-    git submodule update --init --recursive
+    echo "source ~/.dragonbuild/internal/environment" | bash    # dash has failed me
     cd ~
     sudo ln -s ~/.dragonbuild/dragon /usr/local/bin/dragon
 }
